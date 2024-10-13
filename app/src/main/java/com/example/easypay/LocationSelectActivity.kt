081d8cee4896c4c8d3be4cf039b27200ece8a353
Isa8205 <156502124+Isa8205@users.noreply.github.com>
2024-10-13 23:59:23 +0300

    Changes of old

--- app/src/main/java/com/example/easypay/LocationSelectActivity.kt
+++ app/src/main/java/com/example/easypay/LocationSelectActivity.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import org.json.JSONObject
 
 class LocationSelectActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -26,11 +28,29 @@ class LocationSelectActivity : AppCompatActivity() {
 
         val locations = ArrayList<LocationModel>()
 
-        locations.add(LocationModel("Jutmar", "CH7T7VW", "KSH 30"))
-        locations.add(LocationModel("Jutmar", "CH7T7VW", "KSH 30"))
-        locations.add(LocationModel("Jutmar", "CH7T7VW", "KSH 30"))
-        locations.add(LocationModel("Jutmar", "CH7T7VW", "KSH 30"))
-        locations.add(LocationModel("Jutmar", "CH7T7VW", "KSH 30"))
+        val body = JSONObject()
+        body.put("Me", "Ïsa")
+
+        val helper = ApiHelper(applicationContext)
+        val api = "http://192.168.11.67:8000/IsaTest/destinations"
+
+        helper.get(api, object: ApiHelper.CallBack {
+            override fun onSuccess(result: String?) {
+                val locationJson = JSONArray(result.toString())
+
+                (0 until locationJson.length()).forEach {
+                    val location = locationJson.getJSONObject(it)
+
+                    locations.add(
+                        LocationModel(
+                            location.get("name").toString(),
+                            location.get("destination_id").toString(),
+                            "KSH 40"
+                        )
+                    )
+                }
+            }
+        })
 
         val adapter = LocationAdapter(locations)
 
